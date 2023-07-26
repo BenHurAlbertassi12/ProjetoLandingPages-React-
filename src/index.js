a770528d38664a63edb90384d9b15da9fc6a4dbf
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-26 11:19:50 -0300

    Intro Styles Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './templates/App/App.jsx';
+import { GlobalStyles } from './styles/GlobalStyles.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <GlobalStyles />
   </React.StrictMode>,
 );
