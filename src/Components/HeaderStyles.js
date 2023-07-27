a34e0667e07fb14a09588f718d382c5a0b4ee831
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-27 13:19:32 -0300

    Header Styled

--- src/Components/HeaderStyles.js
+++ src/Components/HeaderStyles.js
@@ -1,7 +1,29 @@
+/* eslint-disable no-unused-vars */
 import styled, { css } from 'styled-components';
 
+const titleSize = {
+  big: (theme) => css`
+  font-size: ${theme.font.sizes.xlarge};
+  `,
+  small: (theme) => css`
+  font-size: ${theme.font.sizes.medium};
+  `,
+  medium: (theme) => css`
+  font-size: ${theme.font.sizes.large};
+  `,
+  huge: (theme) => css`
+  font-size: ${theme.font.sizes.xhuge};
+  `,
+};
+
+const titleCase = (uppercase) => css`
+  text-transform: ${uppercase ? 'uppercase' : 'none'};
+`;
+
 export const Title = styled.h1`
-  ${({ theme, light }) => css`
-    color: ${light ? theme.colors.white : theme.colors.primaryColor};
+  ${({ theme, colorDark, size, uppercase }) => css`
+    color: ${colorDark ? theme.colors.primaryColor : theme.colors.white};
+    ${titleSize[size](theme)}
+    ${titleCase(uppercase)}
   `}
 `;
