c3b543f09ec3d810b128ebef3fa33190d9f974a8
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-08-02 12:53:41 -0300

    LogoLink Component

---
+++ src/Components/LogoLink/stories.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { LogoLink } from '.';
+
+export default {
+  title: 'LogoLink',
+  component: LogoLink,
+  args: {
+    children: 'LogoLink',
+  },
+  argTypes: {
+    children: { type: 'string' },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <LogoLink {...args} />
+    </div>
+  );
+};
