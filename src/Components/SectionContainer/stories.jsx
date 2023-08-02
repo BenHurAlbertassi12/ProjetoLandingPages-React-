81e50787d481505124904ed86fdb4018f45b15bf
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-08-02 12:55:44 -0300

    SectionContainer

---
+++ src/Components/SectionContainer/stories.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { SectionContainer } from '.';
+
+export default {
+  title: 'SectionContainer',
+  component: SectionContainer,
+  args: {
+    children: 'SectionContainer',
+  },
+  argTypes: {
+    children: { type: 'string' },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <SectionContainer {...args} />
+    </div>
+  );
+};
